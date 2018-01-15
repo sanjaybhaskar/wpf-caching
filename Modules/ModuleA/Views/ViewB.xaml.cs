7b2751d0e6b0a09faa0f01b519718dfafc01d119
sanjaybhaskar <sanjaybxl@gmail.com>
2018-01-16 00:16:48 +0530

    Removing Items from Cache
    Performance and Memory
    Management

---
+++ Modules/ModuleA/Views/ViewB.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ModuleA.RibbonTabs;
+using Prism.Regions;
+using PrismDemo.Core;
+using System.Windows.Controls;
+
+namespace ModuleA.Views
+{
+    /// <summary>
+    /// Interaction logic for ViewB
+    /// </summary>
+    [DependentView(typeof(ViewBTab), "RibbonTabRegion")]
+    [DependentView(typeof(ViewC), "SubRegion")]
+    [RegionMemberLifetime(KeepAlive = false)]
+    public partial class ViewB : UserControl, ISupportDataContext
+    {
+        public ViewB()
+        {
+            InitializeComponent();
+        }
+    }
+}
